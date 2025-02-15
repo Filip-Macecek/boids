c47cc126fca7a5061d528d4195744da8ddc7dd2b
Filip-Macecek <filip.macecek@mews.com>
2025-02-15 13:01:07 +0100

    alignment

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const BOID_RADIUS = 1;
 const BOID_COLOR = "#000000"
 const BOID_COUNT = 90;
-const BOID_SEPARATION_DISTANCE = 100;
+const BOID_SEPARATION_SQUARE_DISTANCE = 150;
+const BOID_ALIGNMENT_RADIUS = 150;
+const VELOCITY_ALIGNMENT_FACTOR = 0.1;
 
 class Vec2 {
     constructor(x, y) {
@@ -22,8 +24,22 @@ class Vec2 {
         return new Vec2(this.x * n, this.y * n);
     }
 
+    Divide(n)
+    {
+        if (n === 0)
+        {
+            throw Error("Division by zero.");
+        }
+        return new Vec2(this.x / n, this.y / n);
+    }
+
     Square() {
         return new Vec2(this.x ** 2, this.y ** 2);
+    }
+
+    Distance(vec)
+    {
+        return Math.sqrt((this.x - vec.x) ** 2 + (this.y - vec.y) ** 2);
     }
 }
 
@@ -62,7 +78,6 @@ function updateBoids(boids, dt)
 {
     boids.forEach(boid =>
     {
-        let a = boid.speedSec.Multiply(dt);
         let otherBoids = boids.filter(b => b !== boid);
         let sepForce = otherBoids.reduce((acc, otherBoid) => 
         {
@@ -71,7 +86,7 @@ function updateBoids(boids, dt)
             let dSquare = dSquareVec.x + dSquareVec.y;
             let resVector = new Vec2(posDif.x / dSquare, posDif.y / dSquare);
 
-            if (dSquare < BOID_SEPARATION_DISTANCE)
+            if (dSquare < BOID_SEPARATION_SQUARE_DISTANCE)
             {
                 return acc.Add(resVector);
             }
@@ -80,7 +95,18 @@ function updateBoids(boids, dt)
             }
         }, new Vec2(0, 0));
 
+        let alignmentForce = new Vec2(0, 0);
+        let boidsWithinDistance = otherBoids.filter(b => b.pos.Distance(boid.pos) < BOID_ALIGNMENT_RADIUS);
+        if (boidsWithinDistance.length > 0)
+        {
+            let otherBoidsAverageVelocity = boidsWithinDistance.reduce((acc, otherBoid) => acc.Add(otherBoid.speedSec), new Vec2(0, 0)).Divide(boidsWithinDistance.length);
+            alignmentForce = otherBoidsAverageVelocity.Substract(boid.speedSec).Multiply(VELOCITY_ALIGNMENT_FACTOR);
+            // console.log(alignmentForce);
+        }
+
+        let a = boid.speedSec.Multiply(dt);
         a = a.Substract(sepForce);
+        a = a.Add(alignmentForce);
         boid.pos = boid.pos.Add(a);
 
         // prevent going outside of border
