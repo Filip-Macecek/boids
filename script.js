2eb92e15b6ffb69c034463280ec8ff38c76547b2
Filip-Macecek <filip.macecek@mews.com>
2025-02-20 13:53:40 +0100

    Now it looks good sort of

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const BOID_RADIUS = 1;
+const BOID_RADIUS = 5;
 const BOID_COLOR = "#000000"
-const BOID_COUNT = 250;
-const BOID_NEARBY_RADIUS = 80;
-const SEPARATION_FACTOR = 0.2;
-const VELOCITY_ALIGNMENT_FACTOR = 0.02;
-const COHESION_FACTOR = 0.02;
+const BOID_COUNT = 200;
+const SPEED_SCALE = 1;
+var BOID_NEARBY_RADIUS = 120;
+var SEPARATION_FACTOR = 1000;
+var VELOCITY_ALIGNMENT_FACTOR = 1.2;
+var COHESION_FACTOR = 12;
+var MAX_SPEED = 100;
+var GRAVITY = 250000;
 
 class Vec2 {
     constructor(x, y) {
@@ -34,13 +37,28 @@ class Vec2 {
         return new Vec2(this.x / n, this.y / n);
     }
 
-    Square() {
-        return new Vec2(this.x ** 2, this.y ** 2);
-    }
-
     Distance(vec)
     {
         return Math.sqrt((this.x - vec.x) ** 2 + (this.y - vec.y) ** 2);
+    }
+
+    Normalize()
+    {
+        let length = Math.sqrt(this.x ** 2 + this.y ** 2);
+        if (length === 0) {
+            return new Vec2(0, 0);
+        }
+        return new Vec2(this.x / length, this.y / length);
+    }
+
+    Dot(otherVec)
+    {
+        return this.x*otherVec.x + this.y*otherVec.y;
+    }
+
+    Magnitude() 
+    {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
     }
 }
 
@@ -59,9 +77,24 @@ class Boid {
 
     draw(ctx)
     {
+        let angle = Math.atan2(this.speedSec.y, this.speedSec.x) + (Math.PI / 2);
+        ctx.save();
+        ctx.translate(this.pos.x, this.pos.y);
+        ctx.rotate(angle);
         ctx.beginPath();
-        ctx.arc(this.pos.x, this.pos.y, BOID_RADIUS, 0, Math.PI * 2, true);
+        ctx.moveTo(0, -BOID_RADIUS * 2);
+        ctx.lineTo(-BOID_RADIUS, BOID_RADIUS * 2);
+        ctx.lineTo(BOID_RADIUS, BOID_RADIUS * 2);
+        ctx.closePath();
         ctx.fillStyle = BOID_COLOR;
+        ctx.fill();
+        ctx.restore();
+
+        let x = Math.round(this.pos.x);
+        let y = Math.round(this.pos.y);
+        ctx.beginPath();
+        ctx.arc(x, y, 3, 0, Math.PI * 2, true);
+        ctx.fillStyle = "RED";
         ctx.fill();
         ctx.closePath();
     }
@@ -72,11 +105,33 @@ function getRandomVec2(width, height) {
 }
 
 function getRandomVec2NegativeAlso(x, y) {
-    return new Vec2((Math.random() * x * 2) - x, (Math.random() * y * 2) - y);
+    return new Vec2((Math.random() * x * SPEED_SCALE * 2) - x, (Math.random() * y * SPEED_SCALE * 2) - y);
 }
 
-function updateBoids(boids, dt)
+var mid = new Vec2(1280 / 2, 720 / 2);
+function getInitialSpeed(pos) {
+    let boidToMidDir = mid
+        .Substract(pos)
+        .Normalize();
+
+    // tangential speed
+    let clockwiseToMid = new Vec2(-boidToMidDir.x, boidToMidDir.y);
+    
+    let distanceFromMid = mid.Distance(pos);
+    let tangentialSpeed = Math.sqrt(GRAVITY / distanceFromMid);
+    let initialSpeed = clockwiseToMid.Multiply(tangentialSpeed);
+    // console.log(initialSpeed);
+    return initialSpeed;
+}
+
+function updateBoids(boids, dt, ctx)
 {
+    if (dt === 0)
+    {
+        console.log("DELTA TIME IS ZERO.");
+        return;
+    } 
+
     boids.forEach(boid =>
     {
         let otherBoids = boids.filter(b => b !== boid);
@@ -86,43 +141,70 @@ function updateBoids(boids, dt)
         let boidsWithinDistance = otherBoids.filter(b => b.pos.Distance(boid.pos) < BOID_NEARBY_RADIUS);
         if (boidsWithinDistance.length > 0)
         {
-            sepForce = otherBoids.reduce((acc, otherBoid) => 
+            sepForce = boidsWithinDistance.reduce((acc, otherBoid) => 
             {
                 let posDif = otherBoid.pos.Substract(boid.pos);
-                let dSquareVec = posDif.Square();
-                let dSquare = dSquareVec.x + dSquareVec.y;
-                let resVector = new Vec2(posDif.x / dSquare, posDif.y / dSquare);
-                return acc.Add(resVector);
+                return acc.Add(posDif.Divide(posDif.Magnitude() ** 2));
             }, new Vec2(0, 0)).Multiply(SEPARATION_FACTOR);
 
             let otherBoidsAverageVelocity = boidsWithinDistance.reduce((acc, otherBoid) => acc.Add(otherBoid.speedSec), new Vec2(0, 0)).Divide(boidsWithinDistance.length);
             alignmentForce = otherBoidsAverageVelocity.Substract(boid.speedSec).Multiply(VELOCITY_ALIGNMENT_FACTOR);
-            // console.log(alignmentForce);
 
             let otherBoidsAveragePos = boidsWithinDistance.reduce((acc, otherBoid) => acc.Add(otherBoid.pos), new Vec2(0, 0)).Divide(boidsWithinDistance.length);
             cohesionForce = otherBoidsAveragePos.Substract(boid.pos).Multiply(COHESION_FACTOR);
         }
 
-        let a = boid.speedSec.Multiply(dt);
-        a = a.Substract(sepForce);
-        a = a.Add(alignmentForce);
-        a = a.Add(cohesionForce);
-        boid.pos = boid.pos.Add(a);
+        let a = new Vec2(0, 0);
+        a = a.Substract(sepForce.Multiply(dt));
+        a = a.Add(alignmentForce.Multiply(dt));
+        a = a.Add(cohesionForce.Multiply(dt));
 
+        let distanceFromMid = mid.Distance(boid.pos);
+        let boidToMidDir = mid
+            .Substract(boid.pos)
+            .Normalize();
+        let g = GRAVITY / (distanceFromMid ** 2);
+        let gravityAcceleration = boidToMidDir.Multiply(g).Multiply(dt);
+        boid.speedSec = boid.speedSec.Substract(gravityAcceleration).Add(a);
+        if (boid.speedSec.Magnitude() > MAX_SPEED)
+        {
+            boid.speedSec = boid.speedSec.Normalize().Multiply(MAX_SPEED);
+        }
+
+        // if (distanceFromMid < 10)
+        // {
+        //     let drift = g * 250;
+        //     boid.speedSec = boid.speedSec.Substract(boidToMidDir.Multiply(drift).Multiply(dt));
+        // }
+        
         // prevent going outside of border
-        if (boid.pos.x < 0 || boid.pos.x > boid.width)
+        const margin = 50;
+        const fromWallConst = 150000;
+        if (boid.pos.x < margin || boid.pos.x > (boid.width - margin))
         {
-            a.x *= -1;
-            boid.pos = boid.pos.Add(a);
-            boid.speedSec.x *= -1;
+            let fromWall = boid.pos.x < margin ? new Vec2(1, 0) : new Vec2(-1, 0);
+            let fromWallDistance = boid.pos.x < margin ? boid.pos.x : boid.width - boid.pos.x;
+            const fromWallAccel = fromWall.Multiply(fromWallConst).Divide(fromWallDistance ** 2);
+            boid.speedSec = boid.speedSec.Add(fromWallAccel.Multiply(dt))
         }
 
-        if (boid.pos.y < 0 || boid.pos.y > boid.height)
+        if (boid.pos.y < margin || boid.pos.y > (boid.height - margin))
         {
-            a.y *= -1;
-            boid.pos = boid.pos.Add(a);
-            boid.speedSec.y *= -1;
+            let fromWall = boid.pos.y < margin ? new Vec2(0, 1) : new Vec2(0, -1);
+            let fromWallDistance = boid.pos.y < margin ? boid.pos.y : boid.height - boid.pos.y;
+            const fromWallAccel = fromWall.Multiply(fromWallConst).Divide(fromWallDistance ** 2);
+            console.log(fromWallAccel);
+            boid.speedSec = boid.speedSec.Add(fromWallAccel.Multiply(dt))
         }
+
+        boid.pos = boid.pos.Add(boid.speedSec.Multiply(dt));
+
+        ctx.beginPath();
+        ctx.moveTo(boid.pos.x, boid.pos.y);
+        ctx.lineTo(boid.pos.x + gravityAcceleration.x * 100, boid.pos.y + gravityAcceleration.y * 100);
+        ctx.strokeStyle = "orange";
+        ctx.stroke();
+        ctx.closePath();
     });
 }
 
@@ -136,24 +218,40 @@ function start() {
     for (var i = 0; i < BOID_COUNT; i++)
     {
         var pos = getRandomVec2(width, height);
-        var speed = getRandomVec2NegativeAlso(20, 20)
+        // var speed = getRandomVec2NegativeAlso(10, 10)
+        // var boid = new Boid(pos, speed, width, height);
+
+        // var pos = new Vec2((width / 9) * 4, (height / 9) * 4);
+        var speed = getInitialSpeed(pos);
         var boid = new Boid(pos, speed, width, height);
+        
         boids.push(boid);
     }
 
     var lastTime = Date.now();
+
     function loop() {
         var now = Date.now();
         var dt = (now - lastTime) / 1000;
         
         // console.log(dt);
-        updateBoids(boids, dt)
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        updateBoids(boids, dt, ctx);
+
         boids.forEach(boid => boid.draw(ctx));
+        let x = Math.round(mid.x);
+        let y = Math.round(mid.y);
+        ctx.beginPath();
+        ctx.arc(x, y, BOID_RADIUS, 0, Math.PI * 2, true);
+        ctx.fillStyle = "#00FF00";
+        ctx.fill();
+        ctx.closePath();
 
         lastTime = now;
         requestAnimationFrame(loop);
     }
+
+    // setInterval(loop, 100);
 
     loop();
 }
